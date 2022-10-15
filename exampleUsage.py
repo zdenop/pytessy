7681f0023d47f10d349f569e58611d83c3002fb6
zdenop <zdenop@gmail.com>
2022-10-15 13:31:40 +0200

    add OpenCV example

--- exampleUsage.py
+++ exampleUsage.py
@@ -1,31 +1,41 @@
+import cv2
+from PIL import Image, ImageFilter
+
 from source import pytessy
-from PIL import ImageFilter, Image
-
-
-
-
-
 
 if __name__ == "__main__":
 
-	# Create pytessy instance
-	ocrReader = pytessy.PyTessy()
+    # Create pytessy instance
+    ocrReader = pytessy.PyTessy()
 
-	files = ["testImages/testWord.png", "testImages/5.4321.png"]
+    files = ["testImages/testWord.png", "testImages/5.4321.png"]
 
-	for file in files:
-		# Load Image
-		img = Image.open(file)
+    # PIL Example
+    for file in files:
+        # Load Image
+        img = Image.open(file)
+        # Scale up image
+        w, h = img.size
+        img = img.resize((2 * w, 2 * h))
+        # Sharpen image
+        img = img.filter(ImageFilter.SHARPEN)
+        # Convert to ctypes
+        imgBytes = img.tobytes()
+        bytesPerPixel = int(len(imgBytes) / (img.width * img.height))
+        # Use OCR on Image
+        imageStr = ocrReader.read(
+            img.tobytes(),
+            img.width,
+            img.height,
+            bytesPerPixel,
+            raw=True,
+            resolution=600,
+        )
 
-		# Scale up image
-		w, h = img.size
-		img = img.resize((2 * w, 2 * h))
-		# Sharpen image
-		img = img.filter(ImageFilter.SHARPEN)
-		# Convert to ctypes
-		imgBytes = img.tobytes()
-		bytesPerPixel = int(len(imgBytes) / (img.width * img.height))
-		# Use OCR on Image
-		imageStr = ocrReader.read(img.tobytes(), img.width, img.height, bytesPerPixel, raw=True, resolution=600)
+        print(file, imageStr)
 
-		print(file, imageStr)
+    # OpenCV  example
+    img = cv2.imread(files[0])
+    img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    string = ocrReader.read(img.tobytes(), img.shape[1], img.shape[0], 1)
+    print(file, imageStr)
